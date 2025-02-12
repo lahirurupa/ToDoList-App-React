52ceab429b644ab57b6eacdb559f60f93644cf99
lahirurupa <lahirurupasinghe2001@gmail.com>
2025-02-12 14:21:49 +0530

    Add ToDoList.jsx file

---
+++ src/ToDoList.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function ToDoList() {
+    return (
+        <div className='To-Do-List'>
+            <h1>To-Do List</h1>
+            <input type="text" placeholder="Add a task" />
+            <button>Add</button>
+
+            <ul>
+                <li>
+                    Task 1
+                    <button>Delete</button>
+                    <button>👆</button>
+                    <button>👇</button>
+                </li>
+                <li>
+                    Task 2
+                    <button>Delete</button>
+                    <button>👆</button>
+                    <button>👇</button>
+                </li>
+                <li>
+                    Task 3
+                    <button>Delete</button>
+                    <button>👆</button>
+                    <button>👇</button>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default ToDoList;
